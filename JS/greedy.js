cd01e04d452adeabf1ff85e3d33cf175548a8c8d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-31 10:47:20 -0500

    Added logic so function turns change into cents

--- JS/greedy.js
+++ JS/greedy.js
@@ -5,6 +5,14 @@ let change;
 do {
 	change = prompt('Change owed:');
 }
+
 while (change < 0 || isNaN(Number(change)) || isNaN(parseInt(change)));
 
-console.log(change);
+const greedy = (changeOwed) => {
+
+	let cents = Math.round(changeOwed * 100);
+
+	console.log(cents);
+};
+
+greedy(change);
