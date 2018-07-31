8edd1b9b55d37775572a2a0e27fad14aa5406e5a
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-31 10:38:55 -0500

    Program prompts user for change owed until input is valid

--- JS/greedy.js
+++ JS/greedy.js
@@ -1,1 +1,10 @@
 console.log("Greedy!");
+
+let change;
+
+do {
+	change = prompt('Change owed:');
+}
+while (change < 0 || isNaN(Number(change)) || isNaN(parseInt(change)));
+
+console.log(change);
